20ffa051bc4845133f3384551689ffb2c55d9aea
瀬能 忠之 <tseno26@gmail.com>
2017-09-01 09:23:41 +0900

    initial

--- app/index.js
+++ app/index.js
@@ -1,10 +1,24 @@
 const http = require('http');
-const server = http.createServer((req, res) => {
+const {Client} = require('pg');
+const server = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+ 
+  const client = new Client({
+    user: 'postgres',
+    password: 'postgres',
+    host: 'sample.db',
+    database: 'sample'
+  });
+  try {
+    await client.connect();
+    const {rows} = await client.query('select now()');
+    res.end(rows[0]['now'].toString());
+  } catch (e) {
+    res.end(e.stack);
+  }
 });
-
+ 
 const port = 3000;
 server.listen(port, '0.0.0.0', () => {
   console.log(`Server running at http://localhost:${port}/`);
